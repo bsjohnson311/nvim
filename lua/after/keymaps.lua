89a9c5418352553d0ab832164f72f235800396e5
bryce <bsjohnson311@gmail.com>
2023-03-27 22:27:08 -0700

    Added keymap to go to normal mode while in a terminal.

--- lua/after/keymaps.lua
+++ lua/after/keymaps.lua
@@ -11,10 +11,11 @@ vim.keymap.set('n', '<C-j>', "<C-w>j")
 vim.keymap.set('n', '<C-q>', "<C-w>q")
 
 -- Terminal specific window commands
-vim.keymap.set('n', '<C-t>', "<cmd>ToggleTerm direction=horizontal<CR>")
+vim.keymap.set('n', '<C-t>', "<cmd>ToggleTerm direction=horizontal size=20<CR>")
 vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
 vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
 vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
 vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
 vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
 vim.keymap.set('t', '<C-q>', [[<C-\><C-n><C-w><C-q>]])
+vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]])
